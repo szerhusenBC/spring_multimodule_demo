9d80caa484a75d8260c11cf625921d221421ab4b
Stephan Zerhusen <stephan.zerhusen@ewe.de>
2019-05-28 14:58:13 +0200

    initial check in

---
+++ src/main/java/org/zerhusen/multimoduledemo/spring_multimodule_demo/SpringMultimoduleDemoApplication.java
@@ -0,0 +1,13 @@
+package org.zerhusen.multimoduledemo.spring_multimodule_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMultimoduleDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMultimoduleDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/zerhusen/multimoduledemo/spring_multimodule_demo/SpringMultimoduleDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.zerhusen.multimoduledemo.spring_multimodule_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMultimoduleDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
